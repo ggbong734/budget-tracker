d08fda272b372c30e929e30098e9efa6115f180b
Gerry Bong <ggbong734@gmail.com>
2022-03-09 23:59:59 -0800

    first commit

---
+++ src/component/BudgetCard.js
@@ -0,0 +1,16 @@
+import { Card } from "react-bootstrap";
+import { currencyFormatter } from "../utils";
+
+
+export default function BudgetCard({ name, amount, max }) {
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>
+                    <div>{name}</div>
+                    <div>{currencyFormatter.format(amount)} / {currencyFormatter.format(max)}</div>
+                </Card.Title>
+            </Card.Body>
+        </Card>
+    )
+}
